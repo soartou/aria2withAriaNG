8827a0e4c382e5ce5247cd81f0cbc3b989b032ac
soartou <soarzqs@gmail.com>
2018-12-08 19:45:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,8 +5,13 @@ if [ ! -f /config/aria2.conf ]; then
 		echo "rpc-secret=${SECRET}" >> /config/aria2.conf
 	fi
 fi
+if [ ! -f /config/aria2.session ]; then
+	touch /config/aria2.session
+fi
+if [ ! -f /config/dht.dat ]; then
+	cp /conf-copy/dht.dat /config/dht.dat
+fi
 
-touch /config/aria2.session
 
 service mini_httpd restart
 aria2c --conf-path=/config/aria2.conf
